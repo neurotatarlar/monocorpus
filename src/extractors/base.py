b667aec420eb9cc82f5342628dbbd5b725e1f1a5
tans1q <rkharisov.main@gmail.com>
2024-04-15 19:33:33 +0200

    Changed pdf extractor to skip headers, footers, page numbers, etc...

--- src/extractors/base.py
+++ src/extractors/base.py
@@ -1,20 +1,4 @@
-import os.path
-import os.path
 from abc import ABC, abstractmethod
-
-import ebooklib
-from bs4 import BeautifulSoup
-from ebooklib import epub
-from pdfminer.converter import TextConverter
-from pdfminer.layout import LAParams
-from pdfminer.pdfdocument import PDFDocument
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.pdfinterp import resolve1
-from pdfminer.pdfpage import PDFPage
-from pdfminer.pdfparser import PDFParser
-from rich.progress import track
-
-from consts import Dirs
 
 
 class Extractor(ABC):
