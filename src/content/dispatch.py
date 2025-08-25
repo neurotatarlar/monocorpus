6982faa79ea475e80c9d6483ff955059261c40f1
tagay1n <rkharisov.tt@gmail.com>
2025-08-25 08:16:05 +0300

    wip

--- src/content/dispatch.py
+++ src/content/dispatch.py
@@ -151,7 +151,7 @@ class Channel:
             self._dump_to_file("unprocessables", "repairables.txt", self.repairable_docs)
 
     
-def _process_pdf(cli_params, docs_batch_size=48, keys_batch_size=16):
+def _process_pdf(cli_params, docs_batch_size=72, keys_batch_size=18):
     config = read_config()
     stop_event = threading.Event()
     print("Extracting content of pdf documents")
